2960da76d8ab8135d5165a7d697ab9c1966ed6f3
anushka jyotirmayee singh <84956432+anushka0415@users.noreply.github.com>
2023-08-02 17:33:51 +0530

    Add files via upload

---
+++ career_advice/config.py
@@ -0,0 +1,9 @@
+import random
+CONFIG = {
+    "open_ai": {
+        "openai_model_name": "gpt-4",
+        "api-key": "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"
+                   
+    }
+}
+#FOS003047481387
