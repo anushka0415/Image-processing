c56de27fb3bd1439271e7568425cdc671231466a
anushka jyotirmayee singh <84956432+anushka0415@users.noreply.github.com>
2023-08-02 20:16:50 +0530

    Delete career_advice directory

--- career_advice/config.py
+++
@@ -1,9 +0,0 @@
-import random
-CONFIG = {
-    "open_ai": {
-        "openai_model_name": "gpt-4",
-        "api-key": "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"
-                   
-    }
-}
-#FOS003047481387
